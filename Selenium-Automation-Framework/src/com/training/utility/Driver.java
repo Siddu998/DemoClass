bb1d25d5c67189f288bedc94b6c6fb09d71b1da5
rekhadpr <rekha@DESKTOP-42ESNCU>
2022-05-19 16:15:41 +0530

    Framework upload

---
+++ Selenium-Automation-Framework/src/com/training/utility/Driver.java
@@ -0,0 +1,17 @@
+package com.training.utility;
+
+public interface Driver {
+	// KEYS 
+	String CHROME ="webdriver.chrome.driver"; 
+	String FIREFOX ="webdriver.gecko.driver"; 
+	String IE ="webdriver.ie.driver";
+	String PHANTOM="phantomjs.binary.path";
+	String EDGE="webdriver.edge.driver";
+	
+	// PATH 
+	String CHROME_PATH="D:\\SeleniumSofware02112021\\chromedriver_win32\\chromedriver.exe";
+	String FIREFOX_PATH ="";
+	String IE_PATH ="";
+	String PHANTOM_PATH="";
+	
+}
